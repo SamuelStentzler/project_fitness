9d772de4ffb182ffefb9952ce92d0f0e02e996bd
Samuel Stentzler <samuel.stentzler@gmail.com>
2024-11-29 18:12:25 -0300

    modelo apresentacao

--- script.js
+++ script.js
@@ -4,23 +4,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
     navLinks.forEach(link => {
         link.addEventListener("click", (event) => {
-            event.preventDefault(); // Evita o comportamento padrão do link
+            event.preventDefault();
 
-            // Verifica se o link já está ativo
             if (link.classList.contains("active")) {
-                return; // Se já está ativo, não faz nada
+                return;
             }
 
-            // Remove a classe "active" de todos os links
             navLinks.forEach(nav => nav.classList.remove("active"));
 
-            // Adiciona a classe "active" ao link clicado
             link.classList.add("active");
 
-            // Esconde todas as páginas
             pages.forEach(page => page.style.display = "none");
 
-            // Exibe a página correspondente ao link clicado
             const targetPage = document.getElementById(link.getAttribute("data-page"));
             if (targetPage) {
                 targetPage.style.display = "block";
